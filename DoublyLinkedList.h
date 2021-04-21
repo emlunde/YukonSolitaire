8aeda37da7c9af621b0c6fc3e5cbaf9415848a8d
Emil Johannes Lundqvist <emlunde@gmail.com>
2021-04-21 12:11:10 +0200

    started project, and added linkedList ext. libr.

---
+++ DoublyLinkedList.h
@@ -0,0 +1,83 @@
+/* Doubly Linked List implementation */
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct card Card;
+
+struct card
+{
+    char suit;
+    char rank;
+    int visibility;
+};
+
+struct Node  {
+    Card card;
+    struct Node* next;
+    struct Node* prev;
+};
+
+struct Node* head; // global variable - pointer to head node.
+
+//Creates a new Node and returns pointer to it.
+struct Node* GetNewNode(Card x) {
+    struct Node* newNode
+            = (struct Node*)malloc(sizeof(struct Node));
+    newNode->card = x;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+//Inserts a Node at head of doubly linked list
+void InsertAtHead(Card x) {
+    struct Node* newNode = GetNewNode(x);
+    if(head == NULL) {
+        head = newNode;
+        return;
+    }
+    head->prev = newNode;
+    newNode->next = head;
+    head = newNode;
+}
+
+//Inserts a Node at tail of Doubly linked list
+void InsertAtTail(Card x) {
+    struct Node* temp = head;
+    struct Node* newNode = GetNewNode(x);
+    if(head == NULL) {
+        head = newNode;
+        return;
+    }
+    while(temp->next != NULL) temp = temp->next; // Go To last Node
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+//Prints all the elements in linked list in forward traversal order
+void Print() {
+    struct Node* temp = head;
+    printf("Forward: ");
+    while(temp != NULL) {
+        printf("%s ",temp->card.rank + temp->card.suit);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+//Prints all elements in linked list in reverse traversal order.
+void ReversePrint() {
+    struct Node* temp = head;
+    if(temp == NULL) return; // empty list, exit
+    // Going to last Node
+    while(temp->next != NULL) {
+        temp = temp->next;
+    }
+    // Traversing backward using prev pointer
+    printf("Reverse: ");
+    while(temp != NULL) {
+        printf("%s ",temp->card.rank + temp->card.suit);
+        temp = temp->prev;
+    }
+    printf("\n");
+}
--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "DoublyLinkedList.h" //https://gist.github.com/mycodeschool/7429492
 
 int main() {
+
     printf("Hello, World!\n");
     return 0;
 }
+
+struct gameboard
+{
+
+};
