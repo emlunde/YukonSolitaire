cdaf163e76d4fd1ee4e16bf26355af7fd243d5ab
Kasper Baun <Kasperbaun@hotmail.com>
2021-04-29 09:07:54 +0200

    createTestDeck simplified using switch case - not the cleanest of code but it works. Prints out all the assignments for each suit and ranks of each suit.

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include "DoublyLinkedList.h" //
-enum suits  {A,C,D,S};
-
 /*
 Node* newDLL(){
     Node* head = malloc(sizeof (Node));
@@ -11,83 +9,92 @@ Node* newDLL(){
 } */
 
 void createTestDeck(){
-
+    Card new;
     // A setup of for-loops creates the 13 cards from each of the 4 suits
-    for(int x=A; A<=S; x++){
+    for(int index=1; index<=4; index++){
+        if(index==1) new.suit = 'A';
+        if(index==2) new.suit = 'C';
+        if(index==3) new.suit = 'D';
+        if(index==4) new.suit = 'S';
+
         for(int i=0; i<=13; i++){
-            Card new;
+
             // For assigning the aces card in the respective suit
             if(i==0){
-                new.suit = x;
-                new.rank = "0";
-                printf("Printing from aces assign: %c %c \n",new.suit,new.rank);
+                new.rank = '0';
+                printf("Printing from aces assign: %c%c \n",new.suit,new.rank);
             }
                 // For cards 1 through K in the specific suit
-            switch(i) {
+            if(i>0){
+                switch(i) {
                 case 1:
-                    new.rank = "1";
-                    printf("Printing from switch case: %d %c \n",i,new.rank);
+                    new.rank = '1';
+                    printf("Printing from switch case %d: %c%c \n",i,new.suit,new.rank);
                     break;
 
                 case 2:
-                    new.rank = "2";
-                    printf("Printing from switch case: %d %c \n",i,new.rank);
+                    new.rank = '2';
+                        printf("Printing from switch case %d: %c%c \n",i,new.suit,new.rank);
                     break;
 
                 case 3:
-                    new.rank = "3";
-                    printf("Printing from switch case: %d %c \n",i,new.rank);
+                    new.rank = '3';
+                        printf("Printing from switch case %d: %c%c \n",i,new.suit,new.rank);
                     break;
 
                 case 4:
-                    new.rank = "4";
-                    printf("Printing from switch case: %d %c \n",i,new.rank);
+                    new.rank = '4';
+                        printf("Printing from switch case %d: %c%c \n",i,new.suit,new.rank);
                     break;
 
                 case 5:
-                    new.rank = "5";
-                    printf("Printing from switch case: %d %c \n",i,new.rank);
+                    new.rank = '5';
+                        printf("Printing from switch case %d: %c%c \n",i,new.suit,new.rank);
                     break;
 
                 case 6:
-                    new.rank = "6";
-                    printf("Printing from switch case: %d %c \n",i,new.rank);
+                    new.rank = '6';
+                        printf("Printing from switch case %d: %c%c \n",i,new.suit,new.rank);
                     break;
 
                 case 7:
-                    new.rank = "7";
-                    printf("Printing from switch case: %d %c \n",i,new.rank);
+                    new.rank = '7';
+                        printf("Printing from switch case %d: %c%c \n",i,new.suit,new.rank);
                     break;
 
                 case 8:
-                    new.rank = "8";
-                    printf("Printing from switch case: %d %c \n",i,new.rank);
+                    new.rank = '8';
+                        printf("Printing from switch case %d: %c%c \n",i,new.suit,new.rank);
                     break;
 
                 case 9:
-                    new.rank = "9";
-                    printf("Printing from switch case: %d %c \n",i,new.rank);
+                    new.rank = '9';
+                        printf("Printing from switch case %d: %c%c \n",i,new.suit,new.rank);
                     break;
 
                 case 10:
-                    new.rank = "T";
-                    printf("Printing from switch case: %d %c \n",i,new.rank);
+                    new.rank = 'T';
+                        printf("Printing from switch case %d: %c%c \n",i,new.suit,new.rank);
                 break;
 
                 case 11:
-                    new.rank = "J";
-                    printf("Printing from switch case: %d %c \n",i,new.rank);
+                    new.rank = 'J';
+                        printf("Printing from switch case %d: %c%c \n",i,new.suit,new.rank);
                     break;
 
                 case 12:
-                    new.rank = "Q";
-                    printf("Printing from switch case: %d %c \n",i,new.rank);
+                    new.rank = 'Q';
+                        printf("Printing from switch case %d: %c%c \n",i,new.suit,new.rank);
                     break;
 
                 case 13:
-                    new.rank = "K";
-                    printf("Printing from switch case: %d %c \n",i,new.rank);
+                    new.rank = 'K';
+                        printf("Printing from switch case %d: %c%c \n",i,new.suit,new.rank);
                     break;
+
+                default:
+                    printf("Switch case ran default option - something went wrong \n");
+                }
             }
         }
     }
@@ -96,6 +103,5 @@ void createTestDeck(){
 
 int main() {
     createTestDeck();
-    printf("Hello, World!\n");
     return 0;
 }
