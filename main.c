4c11c86cdfc54b095f062763790f896ce435588f
FilipBLJensen <s185133@student.dtu.dk>
2021-04-30 17:22:21 +0200

    Basic structure for gameboard

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
+void printStartBoard(){
+
+    printf("\tC1\tC2\tC3\tC4\tC5\tC6\tC7\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF1\n");
+    printf("\t\t\t\t\t\t\t\t\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF2\n");
+    printf("\t\t\t\t\t\t\t\t\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF3\n");
+    printf("\t\t\t\t\t\t\t\t\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF4\n");
+    printf("\t\t\t\t\t\t\t\t\n");
+    printf("LAST command: \n");
+    printf("Message: \n");
+    printf("INPUT > \n");
+
+}
+
 int main() {
-    printf("Hello, World!\n");
+
+    /*printf("\tC1\tC2\tC3\tC4\tC5\tC6\tC7\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF1\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF2\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF3\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF4\n");
+    printf("LAST command: \n");
+    printf("Message: \n");
+    printf("INPUT > \n");*/
+    printStartBoard();
+
     return 0;
 }
