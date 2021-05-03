ff3cfbe7320a5935625682b5cbf6903bea52736f
FilipBLJensen <s185133@student.dtu.dk>
2021-05-03 09:21:58 +0200

    updatePosition

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
-void printStartBoard(){
+void printStartPosition(){
+
+    printf("\tC1\tC2\tC3\tC4\tC5\tC6\tC7\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF1\n");
+    printf("\t\t\t\t\t\t\t\t\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF2\n");
+    printf("\t\t\t\t\t\t\t\t\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF3\n");
+    printf("\t\t\t\t\t\t\t\t\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF4\n");
+    printf("\t\t\t\t\t\t\t\t\n");
+    printf("LAST command: \n");
+    printf("Message: \n");
+    printf("INPUT > \n");
+
+}
+
+void updatePosition(){
 
     printf("\tC1\tC2\tC3\tC4\tC5\tC6\tC7\n");
     printf("\t\t\t\t\t\t\t\t[]\tF1\n");
@@ -19,15 +36,7 @@ void printStartBoard(){
 
 int main() {
 
-    /*printf("\tC1\tC2\tC3\tC4\tC5\tC6\tC7\n");
-    printf("\t\t\t\t\t\t\t\t[]\tF1\n");
-    printf("\t\t\t\t\t\t\t\t[]\tF2\n");
-    printf("\t\t\t\t\t\t\t\t[]\tF3\n");
-    printf("\t\t\t\t\t\t\t\t[]\tF4\n");
-    printf("LAST command: \n");
-    printf("Message: \n");
-    printf("INPUT > \n");*/
-    printStartBoard();
+    printStartPosition();
 
     return 0;
 }
